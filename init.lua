abbed83996c3c73f3ca39502322f9436f1ed36a4
Henry Oswald <henry@kili.so>
2025-04-22 15:00:38 +0100

    might work with which key still enabled

--- init.lua
+++ init.lua
@@ -166,7 +166,7 @@ vim.keymap.set('n', 'u', '<Nop>', { desc = 'Disable undo key' })
 
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -289,58 +289,23 @@ require('lazy').setup({
   {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    opts = {
-      -- delay between pressing a key and opening which-key (milliseconds)
-      -- this setting is independent of vim.opt.timeoutlen
-      delay = 0,
-      icons = {
-        -- set icon mappings to true if you have a Nerd Font
-        mappings = vim.g.have_nerd_font,
-        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-        -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-        keys = vim.g.have_nerd_font and {} or {
-          Up = '<Up> ',
-          Down = '<Down> ',
-          Left = '<Left> ',
-          Right = '<Right> ',
-          C = '<C-…> ',
-          M = '<M-…> ',
-          D = '<D-…> ',
-          S = '<S-…> ',
-          CR = '<CR> ',
-          Esc = '<Esc> ',
-          ScrollWheelDown = '<ScrollWheelDown> ',
-          ScrollWheelUp = '<ScrollWheelUp> ',
-          NL = '<NL> ',
-          BS = '<BS> ',
-          Space = '<Space> ',
-          Tab = '<Tab> ',
-          F1 = '<F1>',
-          F2 = '<F2>',
-          F3 = '<F3>',
-          F4 = '<F4>',
-          F5 = '<F5>',
-          F6 = '<F6>',
-          F7 = '<F7>',
-          F8 = '<F8>',
-          F9 = '<F9>',
-          F10 = '<F10>',
-          F11 = '<F11>',
-          F12 = '<F12>',
-        },
-      },
+    -- opts = {
+    --   -- delay between pressing a key and opening which-key (milliseconds)
+    --   -- this setting is independent of vim.opt.timeoutlen
+    --   delay = 0,
 
-      -- Document existing key chains
-      spec = {
-        { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      },
-    },
+
+    --   -- Document existing key chains
+    --   spec = {
+    --     { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
+    --     { '<leader>d', group = '[D]ocument' },
+    --     { '<leader>r', group = '[R]ename' },
+    --     { '<leader>s', group = '[S]earch' },
+    --     { '<leader>w', group = '[W]orkspace' },
+    --     { '<leader>t', group = '[T]oggle' },
+    --     { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+    --   },
+    -- },
   },
   {
     'folke/flash.nvim',
