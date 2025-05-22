3f9f30dfefa114232ee9aeefc685cbef5e919ab6
Henry Oswald <henry.oswald@gmail.com>
2025-05-22 13:03:43 +0100

    disable flash

--- init.lua
+++ init.lua
@@ -307,27 +307,27 @@ require('lazy').setup({
     --   },
     -- },
   },
-  {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
-    ---@type Flash.Config
-    -- opts = {
-    -- modes = {
-    -- char = {
-    -- Remove ';' and ',' from the default keys so they can be repeat keys
-    -- keys = { "f", "F", "t", "T" },
-    -- },
-    -- },
-    -- },
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-    },
-  },
+  -- {
+  --   'folke/flash.nvim',
+  --   event = 'VeryLazy',
+  --   ---@type Flash.Config
+  --   -- opts = {
+  --   -- modes = {
+  --   -- char = {
+  --   -- Remove ';' and ',' from the default keys so they can be repeat keys
+  --   -- keys = { "f", "F", "t", "T" },
+  --   -- },
+  --   -- },
+  --   -- },
+  --   -- stylua: ignore
+  --   keys = {
+  --     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+  --     { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+  --     { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+  --     { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  --     { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+  --   },
+  -- },
 
 
   -- Highlight todo, notes, etc in comments
